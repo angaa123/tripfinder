a19fdf4e4416a272154d47e6e58242677b486cac
Toan Vo <vothaitoan12@gmail.com>
2024-05-07 01:29:11 +0700

    feat: add component to reduce repeated code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,5 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
-    "@typescript-eslint/ban-types": "off",
   },
 };
--- src/pages/auth/Login/index.tsx
+++ src/pages/auth/Login/index.tsx
@@ -1,10 +1,10 @@
-import { EyeInvisibleOutlined, EyeOutlined } from "@ant-design/icons";
 import { Carousel, Image } from "antd";
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+import { carouselListImage } from "../../../assets/carouselListImage";
+import InformationInput from "../../../components/InfomationInput";
 
-const Login: React.FC<{}> = () => {
-  const [isVisible, setIsVisible] = useState(false);
+const Login: React.FC = () => {
   return (
     <>
       <div className="flex flex-row items-center gap-8">
@@ -14,44 +14,17 @@ const Login: React.FC<{}> = () => {
           </h3>
           <h2 className="text-2xl font-bold text-start">Login</h2>
           <p className="text-start">Login to access your account</p>
-          <div className="my-2 flex flex-col gap-6">
-            <div className="relative">
-              <input
-                type="text"
-                id="floating_outlined"
-                className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-400 peer"
-                placeholder=" "
-              />
-              <label
-                htmlFor="floating_outlined"
-                className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-green-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-              >
-                Email
-              </label>
-            </div>
-            <div className="relative">
-              <input
-                type={isVisible ? "text" : "password"}
-                id="floating_outlined"
-                className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-400 peer"
-                placeholder=" "
-              />
-              <button
-                className="end-2.5 bottom-2.5 absolute bg-transparent border-none p-0 focus:outline-none"
-                onClick={() => setIsVisible(!isVisible)}
-              >
-                {isVisible ? <EyeOutlined /> : <EyeInvisibleOutlined />}
-              </button>
-              <label
-                htmlFor="floating_outlined"
-                className="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-green-400 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-              >
-                Password
-              </label>
-            </div>
-            <button className="bg-green-400 text-xl p-2 border-none focus:outline-none hover:border-none text-white font-bold">
+          <div className="my-2 flex flex-col gap-5">
+            <InformationInput labelName="Email" />
+            <InformationInput labelName="Password" isPassword={true} />
+            <button className="bg-green-400 text-xl p-2 w-full border-none focus:outline-none hover:border-none text-white font-bold">
               Login
             </button>
+            <Link to={"/"} style={{ textDecoration: "none" }}>
+              <button className="border-green-400 text-xl bg-transparent py-2 w-full hover:border-green-400 focus:outline-none text-green-400 font-semibold">
+                Continue as guest
+              </button>
+            </Link>
             <div>
               Don't have an account ?{" "}
               <Link
@@ -65,34 +38,17 @@ const Login: React.FC<{}> = () => {
         </div>
         <div className="flex-1 max-w-lg">
           <Carousel autoplay>
-            <Image
-              width={500}
-              height={600}
-              preview={false}
-              className="rounded-xl"
-              src="https://images.unsplash.com/photo-1652255432402-4c58931f8ade"
-            />
-            <Image
-              width={500}
-              height={600}
-              preview={false}
-              className="rounded-lg"
-              src="https://images.unsplash.com/photo-1545172538-171a802bd867"
-            />
-            <Image
-              width={500}
-              height={600}
-              preview={false}
-              className="rounded-lg"
-              src="https://images.unsplash.com/photo-1604323990536-e5452c0507c1"
-            />
-            <Image
-              width={500}
-              height={600}
-              preview={false}
-              className="rounded-lg"
-              src="https://images.unsplash.com/photo-1588411393236-d2524cca1196"
-            />
+            {carouselListImage.map((imageLink: string) => {
+              return (
+                <Image
+                  width={500}
+                  height={600}
+                  preview={false}
+                  className="rounded-lg"
+                  src={imageLink}
+                />
+              );
+            })}
           </Carousel>
         </div>
       </div>
