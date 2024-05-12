8a94ed9d71bdb414c855ba76467b43866b13e2fa
Toan Vo <vothaitoan12@gmail.com>
2024-05-13 01:20:38 +0700

    fix: config route

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
-import MultiSelection from "../Home/components/MultiSelection";
+import MultiSelection from "./MultiSelection";
 
 const Explore: React.FC = () => {
   return (
